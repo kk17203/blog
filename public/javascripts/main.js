ee0cff7ece0925af1416cb11a90bf981ebd9a538
kk17203 <kk17203@gmail.com>
2024-01-24 12:30:24 -0600

    Fixed entry expand function. Added toggleFeaturedContent function in main.js for the featured posts.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -30,3 +30,40 @@ function toggleContent(index) {
         }
     });
 }
+
+function toggleFeaturedContent(index) {
+    const button = document.getElementById(`featuredSeeMoreButton${index}`);
+    const previewContent = document.getElementById(
+        `featuredBlogPreviewContent${index}`
+    );
+    const fullContent = document.getElementById(
+        `featuredBlogFullContent${index}`
+    );
+
+    const blogContainer = document.getElementById(
+        `featuredBlogContainer${index}`
+    );
+
+    console.log(fullContent);
+    console.log("test");
+
+    if (fullContent.style.display === "none") {
+        previewContent.style.display = "none";
+        fullContent.style.display = "block";
+        button.innerText = "See Less";
+    } else {
+        previewContent.style.display = "block";
+        fullContent.style.display = "none";
+        button.innerText = "See More";
+    }
+
+    window.addEventListener("click", function (Event) {
+        const isClickInsideContainer = blogContainer.contains(event.target);
+
+        if (!isClickInsideContainer) {
+            previewContent.style.display = "block";
+            fullContent.style.display = "none";
+            button.innerText = "See More";
+        }
+    });
+}
