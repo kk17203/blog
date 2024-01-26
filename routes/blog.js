e503f723d3a269c8c9255b7d9c7b70f205828eaa
kk17203 <kk17203@gmail.com>
2024-01-26 10:35:34 -0600

    Updated title tags for SEO improvement. Added about msg to each blog page.

--- routes/blog.js
+++ routes/blog.js
@@ -19,6 +19,9 @@ router.get(
         }).sort({ createdAt: -1 });
 
         res.render("declutter", {
+            title: "Declutter",
+            message:
+                "As a mom, I understand the constant challenge of keeping our spaces organized and clutter-free. Join me as I explore practical decluttering tips and share our journey towards a more simplified life.",
             posts: posts,
             getPostTextContent: getPostTextContent,
         });
@@ -34,6 +37,9 @@ router.get(
         }).sort({ createdAt: -1 });
 
         res.render("budget", {
+            title: "Budget",
+            message:
+                "Managing a household budget can be a daunting task, but it doesn't have to be. Join me as I share budgeting strategies, frugal living tips, and ways to make the most of your resources.",
             posts: posts,
             getPostTextContent: getPostTextContent,
         });
@@ -49,6 +55,9 @@ router.get(
         }).sort({ createdAt: -1 });
 
         res.render("cooking", {
+            title: "Cooking",
+            message:
+                "Embarking on a culinary adventure, I'm excited to share my kitchen escapades. Join me as I explore the world of cooking from the perspective of a mom who's discovering the joy of creating simple, family-friendly meals. It's all about embracing the learning curve and having fun in the kitchen together!",
             posts: posts,
             getPostTextContent: getPostTextContent,
         });
@@ -64,6 +73,9 @@ router.get(
         }).sort({ createdAt: -1 });
 
         res.render("faith", {
+            title: "Faith",
+            message:
+                "Navigating life with faith is a journey I cherish, and I'm excited to share it with you here on 'Simply Kaitie.' As a Christian, I find solace and purpose in the belief that Jesus is God's son, offering salvation through His boundless love. Join me in weaving faith into our daily routines, exploring prayerful moments, and reflecting on the profound teachings of scripture.",
             posts: posts,
             getPostTextContent: getPostTextContent,
         });
@@ -79,6 +91,9 @@ router.get(
         }).sort({ createdAt: -1 });
 
         res.render("fitness", {
+            title: "Fitness",
+            message:
+                "Balancing a busy family life doesn't mean neglecting personal well-being. Dive into fitness tips, routines, and ideas that cater to moms on the go. Let's stay active together!",
             posts: posts,
             getPostTextContent: getPostTextContent,
         });
