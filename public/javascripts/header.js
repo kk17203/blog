dec4e319e92f0ce0298452fcbbb45c780c1cf733
kk17203 <kk17203@gmail.com>
2024-01-26 13:40:47 -0600

    Fixed side menu not closing all the way.

--- public/javascripts/header.js
+++ public/javascripts/header.js
@@ -11,7 +11,7 @@ function toggleMenu() {
     }
 
     if (sideMenu.style.left === "0px") {
-        sideMenu.style.left = "-200px";
+        sideMenu.style.left = "-215px";
     } else {
         sideMenu.style.left = "0px";
     }
